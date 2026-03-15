505588f20ec107df553f39529795e2a6d238e3ef
Akshat12-cloud <akshatrk37@gmail.com>
2026-03-15 10:03:17 +0530

    update

--- PROBLEMS/Problem1.py
+++ PROBLEMS/Problem1.py
@@ -1,1 +1,14 @@
-print("hello world")
+#Take a 3-digit number and check if all digits are distinct.
+num = input("Enter a 3-digit number: ")
+
+
+if len(num) == 3 and num.isdigit():
+
+    digits = set(num)
+    
+    if len(digits) == 3:
+        print("All digits are distinct.")
+    else:
+        print("Digits are not distinct.")
+else:
+    print("Invalid input. Please enter a 3-digit number.")
