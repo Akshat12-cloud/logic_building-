6d6169d555cac3a172ad8b18c46a4dc3682038ba
Akshat12-cloud <akshatrk37@gmail.com>
2026-03-16 21:26:19 +0530

    upload

---
+++ PROBLEMS/Problem7.py
@@ -0,0 +1,5 @@
+#Take two angles of a triangle and compute the third angle. 
+angle1=int(input("entr the  value for angle 1:"))
+angle2=int(input("enter the value for angle 2:"))
+angle3=180-(angle1+angle2)
+print(f"the third angle of the triangle is {angle3}")
