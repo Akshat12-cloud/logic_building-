ab538492c628049558fb8a16905c83fdfcb4e126
Akshat12-cloud <akshatrk37@gmail.com>
2026-03-17 13:19:03 +0530

    upload

---
+++ PROBLEMS/Problem15.py
@@ -0,0 +1,6 @@
+#Take a weekday number (1–7) and determine if it is a weekday or weekend.
+weekday_no=int(input("enter the no. for weekday (1-7): "))
+if(weekday_no>=1 and weekday_no <=5):
+    print("it is a weekday")
+else:
+    print("it is a weekend")    
