621a4397e8d00dd5431cb010ae53e904743beb83
Akshat12-cloud <akshatrk37@gmail.com>
2026-03-19 14:47:35 +0530

    upload

---
+++ PROBLEMS/Problem23.py
@@ -0,0 +1,10 @@
+#Take three numbers and check if they are in geometric progression.
+a1=int(input("enter the first number:"))
+a2=int(input("enter the second number:"))
+a3=int(input("enter the third number :"))
+
+if (a2/a1) ==(a3/a2):
+    print("the numbers are in geometric progression.")
+
+else:
+    print("the numbers are not in geometric progression.")    
