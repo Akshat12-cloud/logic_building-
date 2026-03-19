dc36c6d9ffcb49cf995de0ff7de7e3021c07b892
Akshat12-cloud <akshatrk37@gmail.com>
2026-03-19 14:54:22 +0530

    upload

---
+++ PROBLEMS/Problem24.py
@@ -0,0 +1,12 @@
+'''Take a 3-digit number and check if the sum of the first and last digit equals the middle 
+digit. '''
+a1=int(input("entr the number :"))
+a2=int(input("enter the number:"))
+a3=int(input("enter the number:"))
+if (a2==a1+a3):
+    print("the sum of the first and last number is equal to the middle number.")
+else:
+    print('the sum of the first and last number is not equal to the middle number.')    
+
+
+
