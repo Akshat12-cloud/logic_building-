8b896906a4b721adabe3e6403c085ad013096787
Akshat12-cloud <akshatrk37@gmail.com>
2026-03-19 14:42:30 +0530

    upload

---
+++ PROBLEMS/Problem22.py
@@ -0,0 +1,9 @@
+#Take three numbers and check if they are in arithmetic progression.
+a1=int(input("enter the first number:"))
+a2=int(input("entr the second number:"))
+a3=int(input("enter the third number :"))
+if (a2-a1)==(a3-a2):
+    print("the three numbers are in arithmetic progresssion.")
+
+else:
+    print("the three numbers are not in arithmetic progression .")    
