dfbabd9a8c10dffe35d1ed12aa3da1c880892369
Akshat12-cloud <akshatrk37@gmail.com>
2026-03-26 23:49:41 +0530

    upload

---
+++ PROBLEMS/Problem55.py
@@ -0,0 +1,6 @@
+#Count how many numbers between 1–500 are divisible by 7 but not by 5.
+count=0
+for i in range(1,501):
+    if (i%7==0) and (i%5!=0):
+        count+=1
+print("Count of numbers between 1 and 500 that are divisible by 7 but not by 5:", count)
