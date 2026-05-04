a69e01197b5f657c963a5d5b66423255e27eded4
Akshat12-cloud <akshatrk37@gmail.com>
2026-05-04 22:13:14 +0530

    upload

--- PROBLEMS/Problem125.py
+++ PROBLEMS/Problem125.py
@@ -1,1 +1,14 @@
-#
+#Check if the array is sorted in descending order. 
+def is_descending(arr):
+    for i in range(len(arr) - 1):
+        if arr[i] < arr[i + 1]:
+            return False
+    return True
+
+# Example
+arr = [9, 7, 5, 3, 1]
+
+if is_descending(arr):
+    print("Array is sorted in descending order.")
+else:
+    print("Array is NOT sorted in descending order.")
